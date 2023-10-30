f3989dd2bc68457839d1b8f145ff2e4ed990048b
Julio Torres <jtorres990@gmail.com>
2023-10-30 11:44:45 -0500

    fix config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const { parsed: env } = require("dotenv").config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    const _entry = config.entry;
-    config.entry = function () {
-      return _entry(...arguments).then((result) => {
-        result["main.js"] = ["/home/juliotorres/htdocs/website/src/index.ts"];
-        //console.log("result", result);
-        return result;
-      });
-    }.bind(config);
-    //config.entry().then(console.log);
     config.plugins.push(new webpack.EnvironmentPlugin(env));
     config.resolve.alias = {
       ...config.resolve.alias,
